5f032d01de8586d254126b890349d155cbb86434
molubhai08 <151838832+molubhai08@users.noreply.github.com>
2026-03-31 00:01:03 +0530

    fixed settings 2

--- researcher/settings.py
+++ researcher/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yrpkk2_xfqq3ybawhibd&3jo84h652gr1cf@=g(l+9=$tv6-_^'
+# SECRET_KEY = 'django-insecure-yrpkk2_xfqq3ybawhibd&3jo84h652gr1cf@=g(l+9=$tv6-_^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = os.environ['SECRET']
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
+
 
 ALLOWED_HOSTS = [
     'axis-b0b5dnaqh6hgcgbz.centralindia-01.azurewebsites.net',
